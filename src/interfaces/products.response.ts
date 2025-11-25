530f81914412b6ec98ccc9cdc4fa0104f6c6508b
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-11-24 21:01:31 -0500

    get products by backend

---
+++ src/interfaces/products.response.ts
@@ -0,0 +1,8 @@
+import type { Product } from "./products.interface";
+
+export interface ProductsResponse {
+  products: Product[]
+}
+
+
+
--- src/shop/pages/home/HomePages.tsx
+++ src/shop/pages/home/HomePages.tsx
@@ -2,8 +2,11 @@ import { CustomPagination } from "@/components/custom/CustomPagination"
 import { products } from "@/mocks/products.mock"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useProducts } from "@/shop/hooks/useProducts"
 
 export const HomePages = () => {
+  const { data } = useProducts();
+  console.log(data);
   return (
     <>
       <CustomJumbotron title="Todos los productos"/>
