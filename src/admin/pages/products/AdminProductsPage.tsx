30df68cc6d1370c3ce83a54ca7cf385ae76b0638
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-12-01 21:06:46 -0500

    action to get product and get products admin

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,11 +1,18 @@
+import { PencilIcon, PlusIcon } from "lucide-react"
+import { Link } from 'react-router';
+
+import {Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
 import { AdminTitle } from "@/admin/components/AdminTitle"
 import { CustomPagination } from "@/components/custom/CustomPagination"
 import { Button } from "@/components/ui/button"
-import {Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
-import { PlusIcon } from "lucide-react"
-import { Link } from "react-router"
+import { useProducts } from "@/shop/hooks/useProducts"
+import { CustomFullScreenLoading } from "@/components/custom/CustomFullScreenLoading";
+import { currencyFormatter } from "@/lib/currency-form";
 
 export const AdminProductsPage = () => {
+  const { data, isLoading } = useProducts();
+  if (isLoading) return <CustomFullScreenLoading />
+  const products = data?.products ?? []
   return (
     <>
 
@@ -34,26 +41,34 @@ export const AdminProductsPage = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>
-              <img src="https://placehold.co/250x250" alt="Product" className="w-20 h-20 object-cover rounded-md" />
-            </TableCell>
-            <TableCell>Producto 1</TableCell>
-            <TableCell>$250.00</TableCell>
-            <TableCell>Categoria 1</TableCell>
-            <TableCell>20</TableCell>
-            <TableCell>XS.SL</TableCell>
-            <TableCell className="text-right">
-              <Link to={`/admin/products/t-shirt-teslo`}>
-                Editar
-              </Link>
-            </TableCell>
-          </TableRow>
+          {
+            products.map((product, i) => (
+              <TableRow key={product.id}>
+                <TableCell className="font-medium">{i+1}</TableCell>
+                <TableCell>
+                  <img src={product.images[0]} alt={product.title} className="w-20 h-20 object-cover rounded-md" />
+                </TableCell>
+                <TableCell>
+                  <Link to={`/admin/products/${product.id}`} className="hover:text-blue-500 underline">
+                    {product.title}
+                  </Link>
+                </TableCell>
+                <TableCell>{currencyFormatter(product.price)}</TableCell>
+                <TableCell>{product.gender}</TableCell>
+                <TableCell>{product.stock} stock</TableCell>
+                <TableCell>{product.sizes.join(', ')}</TableCell>
+                <TableCell className="text-right">
+                  <Link to={`/admin/products/${product.id}`}>
+                    <PencilIcon className="w-4 h-4 text-blue-500" />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))
+          }
         </TableBody>
       </Table>
 
-      <CustomPagination  totalPages={10}/>
+      <CustomPagination  totalPages={data?.pages ?? 1}/>
     </>
   )
 }
